9a267dd05a7bf84b39e1f9d1548deb7d602eb165
Andrew Assarattanakul <assarata@gmail.com>
2022-03-29 13:01:36 -0500

    chore(incomplete work): incomplete work

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,47 @@
 class TasksController < ApplicationController
+
   def index
-    @tasks = Task.all
+    tasks
     @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to root_path, notice: 'Task created' }
+      else
+        tasks
+        format.html { render :index, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    respond_to do |format|
+      if @task.update(update_params)
+        format.html { redirect_to root_path, notice: 'Task Updated' }
+      else
+        tasks
+        format.html { render :index, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def tasks
+    @tasks ||= Task.all.order(:updated_at)
+  end
+
+  def update_params
+    params.require(:task).permit(:done)
+  end
+
+  def task_params
+    params.require(:task).permit(:description)
   end
 end
